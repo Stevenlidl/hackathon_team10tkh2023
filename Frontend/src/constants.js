f1d33cc2227ec1bc0f0275b87875594118096adc
Zixiang Li <36215965+Stevenlidl@users.noreply.github.com>
2023-05-20 14:36:27 -0400

    Update constants.js

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,5 +1,7 @@
 let key = '';
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.GOOGLE_MAP_API_KEY);
 if (process.env.NODE_ENV === 'development') {
     key = "YOUR_API_KEY_HERE"
 } else {
