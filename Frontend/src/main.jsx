ee38f0627bc12c5abf07b187d9c0675e46c4eda3
Dianav21 <dianavargas214@gmail.com>
2023-05-20 16:40:15 -0400

    added home page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import Layout from './pages/layout';
+import Home from './pages/home';
 
 //Creating Router instance
 const router = createBrowserRouter([
@@ -15,7 +16,7 @@ const router = createBrowserRouter([
       //All pages belong here, import component and replace element
       {
         path: "/home",
-        element: <div>Home page</div>
+        element: <Home />
       },
       {
         path: "/map",
