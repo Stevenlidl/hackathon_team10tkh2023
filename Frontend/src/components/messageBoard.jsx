de6af463f2b03eb0bf683226916a3dedd3e947d0
rishchan26 <rishabhchanana@rishabhs-air.myfiosgateway.com>
2023-05-20 14:25:51 -0400

    add scroll

--- Frontend/src/components/messageBoard.jsx
+++ Frontend/src/components/messageBoard.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable react/prop-types */
 import "./messages.css";
 import { MessagePill } from "./messagePill";
+import {useEffect, useRef} from "react";
 
 export function MessageBoard({ messages }) {
+    const boardRef = useRef()
+
+    useEffect(() => {
+        const obj = boardRef.current
+        obj.scrollTo({
+            top: obj.scrollHeight,
+            behavior: "smooth",
+          })
+    });
+
     return (
-        <div className="message-board">
+        <div className="message-board" ref={boardRef}>
             <MessagePill type="sent" by="John Appleseed" message="Lorem ipsum dorem" datetime={new Date().toLocaleString()} />
             <MessagePill type="received" by="John Appleseed" message="Lorem ipsum dorem" datetime={new Date().toLocaleString()} />
             {messages.map((message, index) => (
