61016754ed48699ea51fc6971b4bf0b9a3f91e0f
Zixiang Li <36215965+Stevenlidl@users.noreply.github.com>
2023-05-20 15:04:05 -0400

    Setup API key from env variable

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -3,7 +3,7 @@ let key = '';
 if (process.env.NODE_ENV === 'development') {
     key = "YOUR_API_KEY_HERE"
 } else {
-    key = process.env.GOOGLE_MAP_API_KEY;
+    key = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
 }
 
 export const apiKey = key;
